c2de9d8256661838327b233628b5a0c6a523d10b
BlackServer Owner <91345285+1terty2@users.noreply.github.com>
2021-10-26 12:04:26 +0200

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,14 @@
+@echo off
+title blackkod
+echo.
+:echo xyzakulcsjele 10:
+echo.
+set /p input=ird be a kodot:
+if %input% == xyzakulcsjele10 (
+goto xyzakulcsjele10
+)
+
+:xyzakulcsjele10
+cls
+echo Bent vagy, a weboldal kodja::sxDuas213TZHtasd
+pause >nul
